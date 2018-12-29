51c936faca805102510b82b3fd378c9bf9e79035
Reuben L. Lillie <email@reubenlillie.com>
2018-12-29 11:44:06 -0600

    Update homepage styles and other stylesheets accordingly

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,10 @@
 // Variables
 var navButton = document.querySelector('nav button')
+var prayerWalkDay = document.getElementById('prayerWalkDay')
+var oneDay = 24*60*60*1000 // hours*minutes*seconds*milliseconds
+var skipDays = 3 // Nov 22–24, 2018
+var startPrayerWalk = new Date(2018,07,01) // Aug 1, 2018
+var today = new Date
 
 // Toggle navigation lists
 navButton.addEventListener('click', function(event) {
@@ -7,4 +12,23 @@ navButton.addEventListener('click', function(event) {
 	event.target.setAttribute('aria-expanded', !expanded)
 	var menu = event.target.nextElementSibling
 	menu.classList.toggle('hidden')
-});
+})
+
+// Add Prayer Walk Count
+var count = Math.round(Math.abs((today.getTime() - startPrayerWalk.getTime() - (oneDay * skipDays))/(oneDay)))
+
+function animateCount(elem, start, end, duration) {
+	var range = end - start
+    var current = start
+    var increment = end > start? 1 : -1
+    var stepTime = Math.abs(Math.floor(duration / range))
+    var timer = setInterval(function() {
+        current += increment
+        elem.innerHTML = current
+        if (current == end) {
+            clearInterval(timer)
+        }
+    }, stepTime)
+}
+
+prayerWalkDay.innerHTML = animateCount(prayerWalkDay,0,count,2018)
